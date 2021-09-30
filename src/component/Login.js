c92db35654a80e5447e28331d43f62af5b229360
1neelsabne <niranjan.sabne@neosoftmail.com>
2021-09-30 14:07:05 +0530

    Second Commit

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 
+// Regular expressions for Validations
+
+const regForEmail = RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+const regForCity = RegExp(/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/);
+const regForSate = RegExp(/[A-Z][a-z]+(?: +[A-Z][a-z]+)*/);
+const regForZip = RegExp(/^(\d{4}|\d{6})$/);
+
 // Class Component
 
 export class Login extends Component {
@@ -9,30 +16,75 @@ export class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: [],
+            errors: {
+                email: '',
+                pass: '',
+                city: '',
+                state: '',
+                zip: ''
+            }
         };
     }
 
-    //onChange Event Handler
+    //onChange Event Handler with validation handling
 
     onChangeUser = (event) => {
         const { name, value } = event.target;
+        let errors = this.state.errors;
+        switch (name) {
+            case 'email':
+                errors.email = regForEmail.test(value) ? '' : 'Email is not valid';
+                break;
+            case 'pass':
+                errors.pass = value.length < 8 ? 'Password must be 8 characters long' : '';
+                break;
+            case 'city':
+                errors.city = regForCity.test(value) ? '' : 'City is not valid';
+                break;
+            case 'state':
+                errors.state = regForSate.test(value) ? '' : 'State is not valid';
+                break;
+            case 'zip':
+                errors.zip = regForZip.test(value) ? '' : 'ZipCode is not valid';
+                break;
+        }
         this.setState({ [name]: value });
     }
 
-    //onClick Event Handler
+    //onClick Event Handler with Error Handling
 
     onSubmitButton = (event) => {
         event.preventDefault();
+        event.target.reset();
         let { items, email, pass, city, state, zip } = this.state;
-        this.setState({
-            items: [...items, { email: email, pass: pass, city: city, state: state, zip: zip }]
-        });
+
+        if (this.validate(this.state.errors)) {
+            alert("Valid Form, Please Check Data Below !");
+            this.setState({
+                items: [...items, { email: email, pass: pass, city: city, state: state, zip: zip }]
+            });
+        }
+        else {
+            alert("Invalid Form !!");
+        }
+
+
     };
+
+    //Validating input Lengths
+
+    validate = (errors) => {
+        let valid = true;
+        Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+
+        return valid;
+    }
 
     //Rendering Contents on React (Displaying) 
 
     render() {
+        const { errors } = this.state;
         return (
             <>
                 <div className="p-3 mb-2 bg-warning text-dark">
@@ -55,7 +107,9 @@ export class Login extends Component {
                                                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={this.onChangeUser} required />
                                             </div>
                                             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                            <small id="emailHelp" className="form-text text-danger font-italic">{this.state.errors.email}</small>
                                         </div>
+
 
                                         <div className="col">
                                             <label for="exampleInputPassword1">Password</label>
@@ -63,6 +117,7 @@ export class Login extends Component {
                                                 <span className="input-group-text" id="inputGroupPrepend"><i class="fas fa-key"></i></span>
                                                 <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name="pass" onChange={this.onChangeUser} required />
                                             </div>
+                                            <small id="emailHelp" className="form-text text-danger font-italic">{this.state.errors.pass}</small>
                                         </div>
                                     </div>
                                 </div>
@@ -76,6 +131,7 @@ export class Login extends Component {
                                                 <span className="input-group-text" id="inputGroupPrepend"><i className="fas fa-city"></i></span>
                                                 <input type="text" class="form-control" id="validationDefault03" placeholder="City" name="city" onChange={this.onChangeUser} required />
                                             </div>
+                                            <small id="emailHelp" className="form-text text-danger font-italic">{this.state.errors.city}</small>
                                         </div>
 
                                         <div class="col-md-3 mb-3">
@@ -84,6 +140,7 @@ export class Login extends Component {
                                                 <span className="input-group-text" id="inputGroupPrepend"><i className="fas fa-globe-asia"></i></span>
                                                 <input type="text" class="form-control" id="validationDefault04" placeholder="State" name="state" onChange={this.onChangeUser} required />
                                             </div>
+                                            <small id="emailHelp" className="form-text text-danger font-italic">{this.state.errors.state}</small>
                                         </div>
 
                                         <div class="col-md-3 mb-3">
@@ -92,6 +149,7 @@ export class Login extends Component {
                                                 <span className="input-group-text" id="inputGroupPrepend"><i className="fas fa-map-pin"></i></span>
                                                 <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" name="zip" onChange={this.onChangeUser} required />
                                             </div>
+                                            <small id="emailHelp" className="form-text text-danger font-italic">{this.state.errors.zip}</small>
                                         </div>
                                     </div>
                                 </div>
